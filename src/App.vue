1fc09773cf68142a29338dfd751bcdf865c7ee87
JJHaEun <yj05037@gmail.com>
2023-07-03 16:22:52 +0900

    이벤드 emit사용하기

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <!-- v-bind:하위컴포넌트에서 사용할(받을) props속성이름=”상위컴포넌트 즉, 데이터를 하위로 넘겨주는 상위 컴포넌트의 데이터 이름” -->
-    <AppHeader :str="str"></AppHeader>
+    <!-- 하위 컴포넌트에서 올린 이벤트 받기 =>emit / 받고 그것을 키로 넣어줌. 그리고 원하는 것을 바인딩 해서 넣어줌.-->
+    <AppHeader :str="str" @renew="renewStr"></AppHeader>
   </div>
 </template>
 
@@ -16,6 +17,12 @@ export default {
   components: {
     // "app-header":AppHeader // 변수를 연결
     AppHeader,
+  },
+  methods: {
+    renewStr() {
+      // str의 값을 바꾸기
+      this.str = "Hi,, vue";
+    },
   },
 };
 </script>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <template>
   <header>
     <h1>Header {{ str }}</h1>
+    <button @click="sendEvent">send</button>
   </header>
 </template>
 
@@ -8,6 +9,12 @@
 export default {
   props: {
     str: String,
+  },
+  methods: {
+    sendEvent() {
+      // 상위 컴포넌트에 이벤트 연결(올리기)
+      this.$emit("renew");
+    },
   },
 };
 </script>
